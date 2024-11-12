21920c8d58ab52f33b872fcbec83aad663bbd32e
nelsondev19 <nelsonher019@gmail.com>
2024-11-11 19:55:53 -0600

    [FEAT] Metadata

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,32 @@
+import type { Metadata } from "next";
+
+// COMPONENTS
 import Intro from "@/components/Intro";
 import CEOQuote from "@/components/CEOQuote";
+
+const title = "SnackGraph - Supercharge Your Projects with Our APIs";
+const description =
+  "We offer a set of APIs designed for developers and data enthusiasts who want to access to information easily.";
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    type: "website",
+    locale: "en_US",
+    siteName: "SnackGraph",
+    url: "https://www.snackgraph.dev/",
+  },
+  keywords: [
+    "API",
+    "developers",
+    "GraphQL",
+    "Countries API",
+    "Developer Tools",
+  ],
+};
 
 export default function Home() {
   return (
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 function Navbar() {
   return (
+<<<<<<< HEAD
     <>
       <div className="hidden md:block">
         <nav className="nav-list-float animationFadeInDown max-w-lg rounded-xl border border-gray-200 drop-shadow-sm backdrop-blur-sm bg-white/80">
@@ -22,6 +23,23 @@ function Navbar() {
         </nav>
       </div>
     </>
+=======
+    <div className="hidden md:block">
+      <nav className="nav-list-float animationFadeInDown max-w-lg rounded-xl border border-gray-200 drop-shadow-sm">
+        {paths.map((path) => {
+          return (
+            <Link
+              key={path.id}
+              href={path.path}
+              className="select-none p-2 text-[var(--color-description)] hover:text-[var(--color-title)] duration-300 hover:underline"
+            >
+              {path.name}
+            </Link>
+          );
+        })}
+      </nav>
+    </div>
+>>>>>>> 774098d ([FEAT] Metadata)
   );
 }
 
